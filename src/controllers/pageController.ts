8287ac7e091ba80a8e8170613485e3c3e25d713a
Marcos Aquino <marcoskim60@gmail.com>
2023-02-09 20:20:51 -0300

    Separando as views e modificando os dados de cada página

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,35 @@
 import {Request, Response} from 'express'
 
 export const home = (req: Request, res: Response) => {
-    res.send('home controller');
+    res.render('pages/page', {
+        banner: {
+          title: 'Todos os animais',
+          background: 'allanimals.jpg'
+        }
+    });
 };
 export const dogs = (req: Request, res: Response) => {
-    res.send('dts');
+    res.render('pages/page', {
+      banner: {
+        title: 'Cachorros',
+        background: 'banner_dog.jpg'
+      }
+    });
 };
+
 export const cats = (req: Request, res: Response) => {
-  //res.send(page/page);
+  res.render('pages/page', {
+    banner: {
+      title: 'Gatos',
+      background: 'banner_cat.jpg'
+    }
+  });
 };
 export const fishes = (req: Request, res: Response) => {
-  //res.send(page/page);
+  res.render('pages/page', {
+    banner: {
+      title: 'Peixes',
+      background: 'banner_fish.jpg'
+    }
+  });
 };
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from 'express'
 
 export const search = (req: Request, res: Response) => {
-    //res.send('pages/search);
+    res.send('searched');
 };
