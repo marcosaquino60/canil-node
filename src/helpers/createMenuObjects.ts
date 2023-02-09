4dececd8d089f43c11fc08093fe10ba7d3104477
Marcos Aquino <marcoskim60@gmail.com>
2023-02-09 20:42:47 -0300

    Manipulando o menu ativo no Mustache

---
+++ src/helpers/createMenuObjects.ts
@@ -0,0 +1,16 @@
+type MenuOptions = '' | 'all' | 'dog' | 'cat' | 'fish';
+
+export const createMenuObject = (activeMenu: MenuOptions) => {
+    let returnObjects = {
+      all: false,
+      dog: false,
+      cat: false,
+      fish: false
+    };
+
+    if(activeMenu !== '') {
+        returnObjects[activeMenu] = true;
+    }
+
+    return returnObjects;
+}
